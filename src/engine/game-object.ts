5e31e6d6729e3844636783f6938ee860d5154df7
coronoro <streicher.tim@googlemail.com>
2023-10-03 21:21:46 +0200

    add constructor

--- src/engine/game-object.ts
+++ src/engine/game-object.ts
@@ -1,21 +1,40 @@
-import {Vector} from "./vector";
+import {Coordinate, Vector} from "./vector";
 
 interface GameObject {
     update(delta: number): void;
+
+    addChild(child: GameObject): void;
 }
 
 export class GameEntity implements GameObject {
-    position: Vector = new Vector(0, 0)
+    position: Coordinate = new Coordinate(0, 0)
     scaleX: number = 1
     scaleY: number = 1
     width: number = 0
     height: number = 0
+    children: GameObject[] = []
+
+    constructor(object: Partial<GameEntityParam>) {
+        Object.keys(object).forEach((key ) => {
+            // @ts-ignore //TODO make this better
+            this[key] = object[key]
+        })
+    }
 
     update(delta: number) {
+        this.children.forEach((child) => child.update(delta))
         this.draw();
     }
 
     draw() {
 
     }
+
+    addChild(child: GameObject) {
+        this.children.push(child);
+    }
+}
+
+type GameEntityParam = {
+    [key in keyof GameEntity]: GameEntity[key]
 }
